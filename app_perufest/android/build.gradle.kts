4981ce96cb71105c5c1c3b1430bed90f2ce1b65d
JersonCh <jc2021072619@virtual.upt.pe>
2025-11-25 16:05:12 -0500

    correcciones 2

--- app_perufest/android/build.gradle.kts
+++ app_perufest/android/build.gradle.kts
@@ -12,8 +12,30 @@ subprojects {
     val newSubprojectBuildDir: Directory = newBuildDir.dir(project.name)
     project.layout.buildDirectory.value(newSubprojectBuildDir)
 }
+
 subprojects {
     project.evaluationDependsOn(":app")
+    
+    afterEvaluate {
+        if (project.hasProperty("android")) {
+            val android = project.extensions.getByName("android")
+            if (android is com.android.build.gradle.BaseExtension) {
+                android.apply {
+                    compileSdkVersion(36)
+                    
+                    defaultConfig {
+                        minSdk = 23
+                        targetSdk = 36
+                    }
+                    
+                    compileOptions {
+                        sourceCompatibility = JavaVersion.VERSION_11
+                        targetCompatibility = JavaVersion.VERSION_11
+                    }
+                }
+            }
+        }
+    }
 }
 
 tasks.register<Delete>("clean") {
