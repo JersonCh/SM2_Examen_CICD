0e9376467e101e0f6a0b403439329dae0dd9416e
JersonCh <jc2021072619@virtual.upt.pe>
2025-11-25 16:20:03 -0500

    correciones 6

--- app_perufest/android/build.gradle.kts
+++ app_perufest/android/build.gradle.kts
@@ -1,3 +1,23 @@
+import java.util.Properties
+import java.io.FileInputStream
+import com.android.build.gradle.LibraryExtension
+
+// Provide legacy Flutter extension values for plugins expecting Groovy build scripts
+extensions.extraProperties.apply {
+    set(
+        "flutter",
+        mapOf(
+            "compileSdkVersion" to 36,
+            "compileSdk" to 36,
+            "minSdkVersion" to 23,
+            "minSdk" to 23,
+            "targetSdkVersion" to 36,
+            "targetSdk" to 36,
+            "ndkVersion" to "27.0.12077973"
+        )
+    )
+}
+
 allprojects {
     repositories {
         google()
@@ -15,6 +35,13 @@ subprojects {
 
 subprojects {
     project.evaluationDependsOn(":app")
+
+    plugins.withId("com.android.library") {
+        extensions.configure<com.android.build.gradle.LibraryExtension>("android") {
+            val flutterExt = rootProject.extensions.extraProperties.get("flutter")
+            extensions.extraProperties.set("flutter", flutterExt)
+        }
+    }
 }
 
 tasks.register<Delete>("clean") {
